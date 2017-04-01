36e9ef106589a002ae40db023fb7a71b78f81f6c
m <mmailm@mail.ru>
2017-04-01 22:48:32 +0800

    add fab

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import run, env, cd, prefix, settings
+from fabric.operations import local
+
+env.hosts = ['83.220.170.91']
+env.user = 'light'
+env.activate = 'source ~/.virtualenvs/astro/bin/activate'
+env.use_ssh_config = True
+
+app_dir = "~/projects/aiohttp-astro"
+
+
+def deploy():
+    with cd(app_dir):
+        with prefix(env.activate):
+            run("git pull")
+            with settings(warn_only=True):
+                run('pip install -r requirements.txt')
+                with cd('app/front'):
+                    run('npm install')
+                    run('npm run build')
+                run('kill -HUP /tmp/astro.pid')
+
--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,10 @@
 import aiohttp
 
 bind = '127.0.0.1:8081'
+pidfile = '/tmp/astro.pid'
 proc_name = 'moon'
 workers = 1
 worker_class = 'aiohttp.GunicornWebWorker'
 # accesslog = 'log/access.log'
 # errorlog = 'log/error.log'
+
